bdf4976e9d11a53d4c80f903f9d6c3ede66b1b5d
Ilia <voronkovilya39@gmail.com>
2024-10-06 17:21:15 +0200

    Второе дз

--- main.go
+++ main.go
@@ -4,19 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
-var message string
-
+// requestBody - структура для парсинга JSON запроса
 type requestBody struct {
 	Message string `json:"message"`
 }
 
+// HelloHandler - GET ручка для вывода всех сообщений из БД
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s", message)
+	var messages []Message
+	// Получаем все сообщения из БД
+	DB.Find(&messages)
+
+	// Преобразуем массив сообщений в JSON и отправляем в ответе
+	err := json.NewEncoder(w).Encode(messages)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
+// PostHandler - POST ручка для записи сообщения в БД
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	var body requestBody
 	err := json.NewDecoder(r.Body).Decode(&body)
@@ -24,16 +35,29 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	message = body.Message
-	fmt.Fprintf(w, "Message received: %s", message)
+
+	// Создаем новое сообщение и сохраняем его в базу данных
+	newMessage := Message{Text: body.Message}
+
+	// Проверяем, возникла ли ошибка при добавлении в базу данных
+	err = DB.Create(&newMessage).Error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error inserting message into DB: %v", err)
+		return
+	}
+
+	// Возвращаем подтверждение, что сообщение сохранено
+	fmt.Fprintf(w, "Message received and saved: %s", body.Message)
 }
 
 func main() {
+	InitDB()                   // Инициализация БД
+	DB.AutoMigrate(&Message{}) // Автоматическая миграция структуры Message
+
 	router := mux.NewRouter()
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")   // GET ручка для получения всех сообщений
+	router.HandleFunc("/api/message", PostHandler).Methods("POST") // POST ручка для записи нового сообщения
 
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-
-	router.HandleFunc("/api/message", PostHandler).Methods("POST")
-
-	http.ListenAndServe(":8080", router)
+	log.Fatal(http.ListenAndServe(":8080", router)) // Запускаем сервер на порту 8080
 }
---
+++ orm.go
@@ -0,0 +1,9 @@
+package main
+
+import "gorm.io/gorm"
+
+// Message - структура для хранения сообщений в БД
+type Message struct {
+	gorm.Model
+	Text string `json:"text"` // Наш сервер будет ожидать json с полем text
+}
